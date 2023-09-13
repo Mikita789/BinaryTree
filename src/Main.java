d2e3f2480c71f1ca110b60828285bf4ba96bb78b
Mikita <nikita_popow_1993@mail.ru>
2023-09-13 13:13:19 +0300

    task done

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,130 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BinaryTree bt = new BinaryTree();
+        bt.insertEl(10);
+        bt.insertEl(20);
+        System.out.println(bt.root.value);
+        System.out.println(bt.root.color);
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    }
+}
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
+enum NodeType {
+    RED,
+    BLACK
+}
+
+class NodeBinaryTree {
+    int value;
+    NodeBinaryTree left;
+    NodeBinaryTree right;
+    NodeType color;
+
+    public NodeBinaryTree(int value) {
+        this.value = value;
+    }
+}
+
+class BinaryTree {
+    NodeBinaryTree root;
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void insertEl(int value) {
+        if (root != null) {
+            addElement(value, root);
+        } else {
+            NodeBinaryTree node = new NodeBinaryTree(value);
+            node.color = NodeType.BLACK;
+            root = node;
+        }
+    }
+
+    private void addElement(int value, NodeBinaryTree currentNode) {
+        if (currentNode.value == value) {
+            System.out.println("такой элемент уже есть");
+        } else {
+            if (value < currentNode.value) {
+                if (currentNode.left != null) {
+                    addElement(value, currentNode.left);
+                } else {
+                    NodeBinaryTree node = new NodeBinaryTree(value);
+                    currentNode.left = node;
+                    fixColor(node);
+                }
+            } else if (value > currentNode.value) {
+                if (currentNode.right != null) {
+                    addElement(value, currentNode.right);
+                } else {
+                    NodeBinaryTree node = new NodeBinaryTree(value);
+                    currentNode.right = node;
+                    fixColor(node);
+                }
+            }
+        }
+    }
+
+    public boolean searchElement(int value) {
+        if (root != null) {
+            return searchElementBack(value, root);
+        } else {
+            System.out.println("Дерево пустое");
+            return false;
+        }
+    }
+
+    private boolean searchElementBack(int value, NodeBinaryTree currentNode) {
+        if (currentNode.value == value) {
+            return true;
+        } else if (value < currentNode.value) {
+            if (currentNode.left != null) {
+                return searchElementBack(value, currentNode.left);
+            }
+        } else if (value > currentNode.value) {
+            if (currentNode.right != null) {
+                return searchElementBack(value, currentNode.right);
+            }
+        }
+        return false;
+    }
+
+    private void swapColor(NodeBinaryTree node) {
+        if (node.color == NodeType.RED) {
+            node.color = NodeType.BLACK;
+        } else {
+            node.color = NodeType.RED;
+        }
+    }
+
+    private void fixColor(NodeBinaryTree currentNode) {
+        if (currentNode.value == root.value) {
+            currentNode.color = NodeType.BLACK;
+        } else if (currentNode.color == NodeType.RED && currentNode.left.color == NodeType.RED) {
+            if (currentNode.right.color == NodeType.RED) {
+                swapColor(currentNode);
+            } else {
+                if (currentNode.left.color == NodeType.RED) {
+                    rotateRight(currentNode);
+                    swapColor(currentNode);
+                }
+            }
+        }
+    }
+
+    private void rotateRight(NodeBinaryTree currentNode) {
+        if (currentNode.left == null) {
+            return;
+        }
+
+        NodeBinaryTree leftChild = currentNode.left;
+        currentNode.left = leftChild.right;
+        leftChild.right = currentNode;
+        if (currentNode.value == root.value) {
+            root = leftChild;
         }
     }
 }
+
